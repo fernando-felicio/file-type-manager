1ddb3c9331b07f0d457941add62fc2b0403b8667
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-02 09:13:41 -0300

    Criando pop-up para selecionar diretorio

--- src/application/main.py
+++ src/application/main.py
@@ -1,3 +1,9 @@
 import os
 
 from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title= "Selecione uma pasta: ")
+print(caminho)
+
+lista_arquivo = os.listdir(caminho)
+print(lista_arquivo)
