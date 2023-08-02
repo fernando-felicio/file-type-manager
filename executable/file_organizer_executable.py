1d6031394fc049563a82cfa8ccce4549ab94c0a0
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-02 10:39:08 -0300

    Executavel file_organizer_executable.py

---
+++ executable/file_organizer_executable.py
@@ -0,0 +1,30 @@
+import os
+
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title= "Selecione uma pasta: ")
+print(caminho)
+
+lista_arquivo = os.listdir(caminho)
+print(lista_arquivo)
+
+locais = {
+    "Imagens" : [".jpg", ".jpeg", ".png", ".raw"],
+    "Planilhas" : [".xlss", ".csv"],
+    "Audios" : [".mp3"],
+    "Videos" :[".mp4", ".vid"],
+    "PDF's" : [".pdf"],
+    "Textos" : [".docx"],
+    "Executaveis" : [".exe"],
+    "Compactados":[".rar", ".zip"],
+    "Arquivos SVG" :[".svg"]
+}
+
+for arquivo in lista_arquivo:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
+           
