d79fb4233d6fd42db718fa005c38f14d9f117ae2
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-02 09:42:04 -0300

    Criando diretorios com base nas extensoes

--- src/application/main.py
+++ src/application/main.py
@@ -15,5 +15,15 @@ locais = {
     "Videos" :[".mp4", ".vid"],
     "PDF's" : [".pdf"],
     "Textos" : [".docx"],
-    "Executaveis" : [".exe"]
+    "Executaveis" : [".exe"],
+    "Compactados":[".rar", ".zip"]
 }
+
+for arquivo in lista_arquivo:
+    nome, extensao = os.path.splitext = (f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.makedir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/ {arquivo}", f"{caminho}/{pasta}/{arquivo}")
+           
